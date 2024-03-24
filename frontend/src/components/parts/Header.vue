e3ad7788e2824b900ecae33c49e5859f3b5ddd3b
かまりょ２ <ryouhei.kamagata@icloud.com>
2024-03-24 13:33:07 +0900

    frontendの追加

---
+++ frontend/src/components/parts/Header.vue
@@ -0,0 +1,15 @@
+<script></script>
+
+<template>
+  <v-app-bar color="primary">
+    <template v-slot:append>
+      <v-icon>mdi-home</v-icon>
+    </template>
+
+    <template v-slot:title>
+      <v-app-bar-title class="font-weight-bold">Todo管理</v-app-bar-title>
+    </template>
+  </v-app-bar>
+</template>
+
+<style></style>
