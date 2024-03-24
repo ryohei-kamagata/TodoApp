43713d015fcca0c9846e3566b04cb43052e411c1
かまりょ２ <ryouhei.kamagata@icloud.com>
2024-03-24 12:08:06 +0900

    レスポンスに天気・気温・地域名を追加

--- backend/app/main.py
+++ backend/app/main.py
@@ -11,4 +11,12 @@ app = FastAPI()
 def read_root():
     url = "https://www.jma.go.jp/bosai/forecast/data/forecast/130000.json"
     r = requests.get(url).json()
-    return {"response": r[0]["timeSeries"][0]["areas"][0]["area"]["name"]}
+    weather = r[0]["timeSeries"][0]["areas"][0]["weathers"][0]
+    temp =r[0]["timeSeries"][2]["areas"][0]["temps"][0]
+    place = r[0]["timeSeries"][0]["areas"][0]["area"]["name"]
+    response = {
+        "weather": weather,
+        "temperture": temp,
+        "place": place
+    }
+    return {"response": response}
